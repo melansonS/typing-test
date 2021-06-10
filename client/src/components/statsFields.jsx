fae0106b4dba2a10e5cba04b72bd9becf16b31bd
Sam <samuelmelanson@live.com>
2021-06-10 14:32:00 -0400

    added debounce on cpm and wpm stats! :tada:

--- client/src/components/statsFields.jsx
+++ client/src/components/statsFields.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './statsFields.css';
+import React, { useCallback, useEffect, useState } from "react";
+import _ from "lodash";
+import PropTypes from "prop-types";
+import "./statsFields.css";
 
 const StatsFields = (props) => {
+  const { characters, seconds } = props;
+  const [debouncedCharacters, setDebouncedCharacters] = useState(0);
+
+  const debouce = useCallback(
+    _.debounce((chars) => {
+      setDebouncedCharacters(chars);
+    }, 500),
+    []
+  );
+
+  useEffect(() => {
+    debouce(characters);
+  }, [characters]);
+
   const roundedCPM = (c, s) => Math.floor((c / (60 - s)) * 60);
   const roundedWPM = (c, s) => Math.floor(((c / (60 - s)) * 60) / 5);
-
-  const { characters, seconds } = props;
 
   return (
     <div className="stat-field-container">
@@ -17,15 +30,17 @@ const StatsFields = (props) => {
       <div className="stat-field">
         Words:
         <span className="stat-number">
-          {roundedWPM(characters, seconds) === Infinity ? 0
-            : roundedWPM(characters, seconds) || 0}
+          {roundedWPM(debouncedCharacters, seconds) === Infinity
+            ? 0
+            : roundedWPM(debouncedCharacters, seconds) || 0}
         </span>
       </div>
       <div className="stat-field">
         Characters:
         <span className="stat-number">
-          {roundedCPM(characters, seconds) === Infinity ? 0
-            : roundedCPM(characters, seconds) || 0}
+          {roundedCPM(debouncedCharacters, seconds) === Infinity
+            ? 0
+            : roundedCPM(debouncedCharacters, seconds) || 0}
         </span>
       </div>
     </div>
